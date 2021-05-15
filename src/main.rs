165491c51e7e647cfc010ef2005a2ac66c4b0b06
Shivansh-Potdar <loldude482@gmail.com>
2021-05-15 17:05:34 +0530

    TUI:Compat

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 fn main(){
-    manga::run();
+    run().unwrap();
+
+}
+
+#[tokio::main]
+async fn run() -> Result<(), Box<dyn std::error::Error>>{
+    manga::run().await?;
+    Ok(())
 }
 
 mod manga {
@@ -7,6 +14,8 @@ mod manga {
     use serde_json::{Value};
     use serde::Deserialize;
     use std::collections::LinkedList;
+    use crate::tui;
+
 
     #[derive(Debug, Deserialize)]
     struct DataHolder {
@@ -14,10 +23,10 @@ mod manga {
         r#type: String
     }
 
-    pub fn run() -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn run() -> Result<(), Box<dyn std::error::Error>> {
         println!("Running API");
-        let res = reqwest::blocking::get("https://api.mangadex.org/manga/random")?;
-        let body = res.text()?;
+        let res = reqwest::get("https://api.mangadex.org/manga/79eb00db-ca63-46e6-8041-36f808146aaa").await?;
+        let body = res.text().await?;
     
         let v: Value = serde_json::from_str(&body[..])?;
         let rel_val: String = v["relationships"].to_string();
@@ -26,6 +35,8 @@ mod manga {
 
         //insert pages here
         let mut _pages: HashMap<String, Vec<String>> = HashMap::new();
+
+        tui::run_load_tui(chapter_links);
 
         Ok(())
     }
@@ -103,5 +114,63 @@ mod manga {
 //maybe write all base urls with pages to .txt in order and just load em up
 
 mod tui {
+    use cursive::align::HAlign;
+    use cursive::event::EventResult;
+    use cursive::traits::*;
+    use cursive::views::{Dialog, OnEventView, SelectView, TextView};
+    use cursive::Cursive;
 
+    // We'll use a SelectView here.
+    //
+    // A SelectView is a scrollable list of items, from which the user can select
+    // one.
+
+    pub fn run_load_tui(v: std::collections::LinkedList<String>) {
+        println!("Loading the interface");
+        let mut select = SelectView::new()
+            // Center the text horizontally
+            .h_align(HAlign::Center)
+            // Use keyboard to jump to the pressed letters
+            .autojump();
+
+        // Read the list of mangas from separate file, and fill the view with it.
+        // (We include the file at compile-time to avoid runtime read errors.)
+        //let content = include_str!("D:\\RustPr\\manga_reader\\src\\chaps.txt");
+        select.add_all_str(v.iter());
+
+        // Sets the callback for when "Enter" is pressed.
+        select.set_on_submit(show_next_window);
+
+        // Let's override the `j` and `k` keys for navigation
+        let select = OnEventView::new(select)
+            .on_pre_event_inner('k', |s, _| {
+                let cb = s.select_up(1);
+                Some(EventResult::Consumed(Some(cb)))
+            })
+            .on_pre_event_inner('j', |s, _| {
+                let cb = s.select_down(1);
+                Some(EventResult::Consumed(Some(cb)))
+            });
+
+        let mut siv = cursive::default();
+
+        // Let's add a ResizedView to keep the list at a reasonable size
+        // (it can scroll anyway).
+        siv.add_layer(
+            Dialog::around(select.scrollable().fixed_size((20, 10)))
+                .title("Pick The Chapter"),
+        );
+
+        siv.run();
+    }
+
+    // Let's put the callback in a separate function to keep it clean,
+    // but it's not required.
+    fn show_next_window(siv: &mut Cursive, city: &str) {
+        siv.pop_layer();
+        let text = format!("{} is a great manga!", city);
+        siv.add_layer(
+            Dialog::around(TextView::new(text)).button("Quit", |s| s.quit()),
+        );
+    }
 }
